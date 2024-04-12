23e92e58f9a4a53033aca113ae887c84d6413816
Ashish Jain <ashxjain@gmail.com>
2024-04-12 22:39:26 +0530

    Rename test to test_model

--- common/models/resnet.py
+++ common/models/resnet.py
@@ -79,9 +79,9 @@ def ResNet34():
     return ResNet(BasicBlock, [3, 4, 6, 3])
 
 
-def test():
+def test_model():
     net = ResNet18()
     y = net(torch.randn(1, 3, 32, 32))
     print(y.size())
 
-# test()
+# test_model()
